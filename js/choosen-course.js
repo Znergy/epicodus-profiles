42d94b4e083de53a687d18d7818734413ed82055
Ryan <ryanjonesirl@gmail.com>
2017-03-13 13:04:44 -0700

    add the logic to to apply an image place holder

--- js/choosen-course.js
+++ js/choosen-course.js
@@ -1,23 +1,48 @@
 $(function() {
   if(window.location.href.includes("java")) {
     $("#courseTitle").text("JAVA");
-    var photoCount = $(".photo").length;
-    console.log("number of photos: " + photoCount);
-    var javaPersonImg = "";
+    setBlankImages();
+//    setImages("java"); // how to set all images
+  } else if (window.location.href.includes("ruby")) {
+    $("#courseTitle").text("RUBY");
+    setBlankImages();
+    //    setImages("ruby"); // how to set all images
+  } else if (window.location.href.includes("design")) {
+    $("#courseTitle").text("DESIGN");
+    setBlankImages();
+    //    setImages("design"); // how to set all images
+  } else if (window.location.href.includes("csharp")) {
+    $("#courseTitle").text("C#/.NET");
+    setBlankImages();
+    //    setImages("csharp"); // how to set all images
+  } else if (window.location.href.includes("php")) {
+    $("#courseTitle").text("PHP");
+    setBlankImages();
+    //    setImages("php"); // how to set all images
+  }
+});
+
+function setBlankImages() {
+  var photoCount = $(".photo").length;
+    var PersonImg = "";
     for(i=0;i <= photoCount; i++) {
-      javaPersonImg = "img/java-student" + i + ".png";
+      PersonImg = "img/grey-person.png";
       $("#student" + i).attr({
-        src: javaPersonImg,
+        src: PersonImg,
+        alt: 'grey person'
+      });
+    }
+}
+
+
+function setImages(name) {
+  var photoCount = $(".photo").length;
+    var PersonImg = "";
+    for(i=0;i <= photoCount; i++) {
+      PersonImg = "img/" + name + "-student" + i + ".png";
+      $("#student" + i).attr({
+        src: PersonImg,
         alt: 'student' + i
       });
     }
-  } else if (window.location.href.includes("ruby")) {
-    $("#courseTitle").text("RUBY");
-  } else if (window.location.href.includes("design")) {
-    $("#courseTitle").text("DESIGN");
-  } else if (window.location.href.includes("csharp")) {
-    $("#courseTitle").text("C#/.NET");
-  } else if (window.location.href.includes("php")) {
-    $("#courseTitle").text("PHP");
-  }
-});
+}
