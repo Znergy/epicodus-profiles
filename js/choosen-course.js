350056876a5a5672d5fff273a68dd1e78c67fb34
Ryan <ryanjonesirl@gmail.com>
2017-03-12 17:51:38 -0700

    add logic to take all photos associated with java class and append them to the screen accordingly, this is done using some new things, like counting total amount of element with a certain class, then using that number to know how many times to loop

--- js/choosen-course.js
+++ js/choosen-course.js
@@ -1,6 +1,16 @@
 $(function() {
   if(window.location.href.includes("java")) {
     $("#courseTitle").text("JAVA");
+    var photoCount = $(".photo").length;
+    console.log("number of photos: " + photoCount);
+    var javaPersonImg = "";
+    for(i=0;i <= photoCount; i++) {
+      javaPersonImg = "img/java-student" + i + ".png";
+      $("#student" + i).attr({
+        src: javaPersonImg,
+        alt: 'student' + i
+      });
+    }
   } else if (window.location.href.includes("ruby")) {
     $("#courseTitle").text("RUBY");
   } else if (window.location.href.includes("design")) {
